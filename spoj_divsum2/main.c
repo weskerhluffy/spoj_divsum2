61e3720e7703efef43b90c3e6b31351bb62ab100
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-12-07 07:59:19 -0600

    Initial Commit

---
+++ spoj_divsum2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  spoj_divsum2
+//
+//  Created by ernesto alvarado on 07/12/19.
+//  Copyright © 2019 ernesto alvarado. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
